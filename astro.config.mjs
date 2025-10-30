dd0022ee89b058cb6bd152834449f8808b274c2e
Макс <maks@MacBook-Pro--Elfenomeno.local>
2025-10-30 19:28:59 +0300

    Верстка шаблона

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // Используем @use вместо @import
+          additionalData: `
+            
+            @use "/src/styles/global" as *;
+          `
+        }
+      }
+    }
+  }
+});
