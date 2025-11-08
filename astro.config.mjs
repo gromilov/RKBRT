7a2f564a8b669dc7ab0e58075c96d4c0eafec3ad
Громилов Максим, фронтенд подрядчик, команда Жукова Д <gromilov.m@etton.net>
2025-11-08 23:02:40 +0300

    fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,35 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
+  build: {
+    assets: 'assets',
+  },
+
+  
   vite: {
-    scopedStyleStrategy: 'none',
+    build: {
+      minify: false,
+      cssCodeSplit: false, // Отключаем разделение CSS
+      rollupOptions: {
+        output: {
+          // Единый файл для JS
+          entryFileNames: 'js/[name].js',
+          
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name?.endsWith('.css')) {
+              return 'css/style.css'; // Единый файл для CSS
+            }
+            return 'images/[name].[ext]';
+          }
+        }
+      }
+    },
     css: {
+      minify: false,
       preprocessorOptions: {
         scss: {
-          // Используем @use вместо @import
           additionalData: `
-            
             @use "/src/styles/variables" as *;
           `
         }
